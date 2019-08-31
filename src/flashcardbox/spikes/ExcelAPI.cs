2f31288bcc283ac7fb4d1da2dfd36eb407602a05
Ralf Westphal <info@ralfw.de>
2019-08-31 10:47:43 +0200

    Excel API spike

---
+++ src/flashcardbox/spikes/ExcelAPI.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace spikes
+{
+    public class UnitTest1
+    {
+        public class FlashcardRecord {
+            public string Id { get; set; }
+            public string Question { get; set; }
+            public string Answer { get; set; }
+            public string Tags { get; set; }
+            public string BinIndex { get; set; }
+        }
+        
+        
+        
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        
+        [Fact]
+        public void Test1()
+        {
+            using (var wb = new XLWorkbook("flashcards.xlsx"))
+            {
+                var records = new List<FlashcardRecord>();
+                
+                var ws = wb.Worksheets.First();
+                var rowNumber = 0;
+                foreach (var row in ws.RowsUsed()) {
+                    if (rowNumber > 0) {
+                        records.Add(new FlashcardRecord {
+                            Id = GetCell(row, "E"),
+                            Question = GetCell(row, "A"),
+                            Answer = GetCell(row, "B"),
+                            Tags = GetCell(row, "C"),
+                            BinIndex = GetCell(row, "D")
+                        });
+                    }
+                    rowNumber++;
+                }
+                
+                foreach(var rec in records)
+                    _testOutputHelper.WriteLine($"{rec.Id}: {rec.Question}={rec.Answer}, {rec.Tags}/{rec.BinIndex}");
+
+                foreach (var rec in records)
+                {
+                    if (rec.Id == "")
+                        rec.Id = Guid.NewGuid().ToString();
+                    if (rec.BinIndex == "")
+                        rec.BinIndex = Guid.NewGuid().ToString().Substring(0, 2);
+                }
+
+                rowNumber = 0;
+                foreach (var row in ws.RowsUsed()) {
+                    if (rowNumber > 0)
+                    {
+                        var fc = records[rowNumber - 1];
+                        row.Cell("E").Value = fc.Id;
+                        row.Cell("A").Value = fc.Question;
+                        row.Cell("B").Value = fc.Answer;
+                        row.Cell("C").Value = fc.Tags;
+                        row.Cell("D").Value = fc.BinIndex;
+                    }
+                    rowNumber++;
+                }
+                 
+                wb.Save();
+            }
+
+
+            string GetCell(IXLRow row, string columnName)
+            {
+                try
+                {
+                    return row.Cell(columnName).GetString().Replace("\n", "$");
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
